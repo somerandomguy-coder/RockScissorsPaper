0edecda8aa88962624cf948c173429c2904bea2e
somerandomguy-coder <153010587+somerandomguy-coder@users.noreply.github.com>
2024-03-26 16:28:10 +1100

    Refine code make it readable, create GUI

--- jsFile/logic.js
+++ jsFile/logic.js
@@ -1,49 +1,133 @@
-function setUp(){
-    let computerGuess = Math.floor(Math.random()*3);
+function play(userGuess, computerGuess){
+    switch (userGuess.toLowerCase()){
+            case "rock":
+                switch (computerGuess){
+                    case 0:
+                        return "Tie";
+                       
+                    case 1:
+                        return "You win!";
+                       
+                    case 2:
+                        return "You lose!";
+                }
+                break;
+            case "paper":
+                switch (computerGuess){
+                    case 0:
+                        return "You win!";
+                       
+                    case 1:
+                        return "You lose!";
+                       
+                    case 2:
+                        return "Tie";
+                }
+                break;
+            case "scissor":
+                switch (computerGuess){
+                    case 0:
+                        return "You lose!";
+                       
+                    case 1:
+                        return "Tie";
+                       
+                    case 2:
+                        return "You win!";
+                }
+            default:
+                return "wrong! word! BITCH!!!";
+    }
+}
+
+function checkScore(){
+    console.log("bar");
+    let win = 0;
+    let tie = 0;
+    let lose = 0;
+    for (const index in lst){
+        if (lst[index] == 'W'){
+            win++;
+        } else if (lst[index] == 'L'){
+            lose++;
+        } else {tie++;}
+    }
+    if (win>lose){
+        winLoseTie = "win";
+    } else if (win<lose){
+        winLoseTie = "lose";
+    } else {
+        winLoseTie = "tie";
+    }
+    return [win, tie, lose, winLoseTie];
+}
+
+function checkAndUpdate(){
+    let status = document.querySelector(".status");
+    result = play(userGuess, computerGuess);
+    status.textContent = result;
+    switch(result){
+        case "You win!":
+            lst.push("W");
+            break;
+        case "You lose!":
+            lst.push("L");
+            break;
+        case "Tie":
+            lst.push("T");
+            break;
+    }
+    container.children[lst.length-1].textContent = lst[lst.length-1];
+    if (lst.length == 5){
+        input.disabled = true;
+        btn.disabled = true;
+        let [win, tie, lose, winloseTie] = checkScore();
+        status.textContent = `End game!!! The final result is you ${winloseTie} with a score of ${win} win, ${lose} lose, ${tie} tie`;
+        newGameBtn.style.display = "block";
+    }
+}
+
+
+
+function initialize(){
+    computerGuess = Math.floor(Math.random()*3);
     /*
     0: rock
     1: scissor
     2: paper
      */
-    let userGuess = prompt("Put your choice here: ");
-    switch (userGuess.toLowerCase()){
-        case "rock":
-            switch (computerGuess){
-                case 0:
-                    alert("Draw");
-                    break;
-                case 1:
-                    alert("You win!");
-                    break;
-                case 2:
-                    alert("You lose!");
-            }
-            break;
-        case "paper":
-            switch (computerGuess){
-                case 0:
-                    alert("You win!");
-                    break;
-                case 1:
-                    alert("You lose!");
-                    break;
-                case 2:
-                    alert("Draw");
-            }
-            break;
-        case "scissor":
-            switch (computerGuess){
-                case 0:
-                    alert("You lose!");
-                    break;
-                case 1:
-                    alert("Draw");
-                    break;
-                case 2:
-                    alert("You win!");
-            }
-        default:
-            alert("wrong word my friend! (rock, scissor, paper)")
-    }
-}
-setUp()
+}   
+
+
+
+let input, btn, userGuess, computerGuess, status;
+let lst = [];
+let container = document.querySelector("#box_container");
+let newGameBtn = document.querySelector(".newGame");
+
+
+//play
+console.log(container.children);
+btn = document.querySelector("button");
+input = document.querySelector("input");
+initialize();
+btn.addEventListener("click", ()=>{
+    userGuess = input.value;
+    checkAndUpdate();
+    console.log(lst);
+    initialize();
+    input.value = "";
+});
+
+//rematch (new game)
+newGameBtn.addEventListener("click", ()=>{
+    lst = [];
+    let boxes = document.querySelectorAll(".box");
+    boxes.forEach((box) => {
+        box.textContent = "";
+      });
+    input.disabled = false;
+    btn.disabled = false;
+})
+
+
